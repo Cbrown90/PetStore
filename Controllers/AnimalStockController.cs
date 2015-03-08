b9ab2675a43c2f448bd57fcde38d3d67bc8481ce
Cbrown90 <christiankennethbrown@gmail.com>
2015-03-08 19:03:32 +0000

    Added DB Changes and Basic Controllers

---
+++ Controllers/AnimalStockController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Controllers
+{
+    public class AnimalStockController : Controller
+    {
+        //
+        // GET: /AnimalStock/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
