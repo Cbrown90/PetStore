efed52b6f3c355869cc3b9deda78a4af5feb8054
Cbrown90 <christiankennethbrown@gmail.com>
2015-03-09 18:03:38 +0000

    Models added, more views and controllers

--- Controllers/AnimalStockController.cs
+++ Controllers/AnimalStockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PetStore.Models;
 
 namespace PetStore.Controllers
 {
@@ -12,6 +13,25 @@ namespace PetStore.Controllers
         // GET: /AnimalStock/
 
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddAnimal()
+        {
+            var Animal = new Animal();
+            return View(Animal);
+        }
+
+        public ActionResult EODReport()
+        {
+            Entities context = new Entities();
+            var Animals = context.Animals.ToList();
+
+            return View(Animals);
+        }
+
+        public ActionResult SellAnimal()
         {
             return View();
         }
---
+++ Controllers/VaccinationCheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Controllers
+{
+    public class VaccinationCheckController : Controller
+    {
+        // GET: VaccinationCheck
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VaccinateAnimal()
+        {
+            return View();
+        }
+    }
+}
